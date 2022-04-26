8443458527251239a171b5a76b096dcc30413a1c
olegcntu <oleg.krasavcik.41@gmail.com>
2022-04-26 12:16:29 +0300

    DB is work successfully

---
+++ src/main/java/com/project/entity/AuthorEntity.java
@@ -0,0 +1,86 @@
+package com.project.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "author", schema = "public", catalog = "forum")
+public class AuthorEntity {
+    private long id;
+    private String name;
+    private Long age;
+    private String about;
+
+    @Override
+    public String toString() {
+        return "AuthorEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", about='" + about + '\'' +
+                '}';
+    }
+
+    @Id
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "age")
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "about")
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AuthorEntity that = (AuthorEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (age != null ? !age.equals(that.age) : that.age != null) return false;
+        if (about != null ? !about.equals(that.about) : that.about != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (about != null ? about.hashCode() : 0);
+        return result;
+    }
+}
